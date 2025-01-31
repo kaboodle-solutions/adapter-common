ad50b7414e3acea181f68663eaed6427b70cee9d
John Mitchell <john.mitchell@kaboodle.co.uk>
2025-01-31 16:39:27 +0000

    Do not remove list after deleting keys

--- AbstractCachePool.php
+++ AbstractCachePool.php
@@ -354,17 +354,7 @@ abstract class AbstractCachePool implements PhpCachePool, LoggerAwareInterface, 
         }
 
         // Remove all items with the tag
-        $success = $this->deleteItems($itemIds);
-
-        if ($success) {
-            // Remove the tag list
-            foreach ($tags as $tag) {
-                $this->removeList($this->getTagKey($tag));
-                $l = $this->getList($this->getTagKey($tag));
-            }
-        }
-
-        return $success;
+        return $this->deleteItems($itemIds);
     }
 
     public function invalidateTag($tag)
